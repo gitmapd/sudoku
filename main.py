960a5585a954ff87fad9ff57014285addfa4288a
gitmapd <50059062+gitmapd@users.noreply.github.com>
2022-03-26 22:05:29 +0000

    Change to QLineEdit and random

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QLabel
-from PyQt5.QtWidgets import QWidget, QGridLayout, QLabel
+from PyQt5.QtWidgets import QWidget, QGridLayout, QLineEdit
+from random import randrange
 app=QApplication(sys.argv)
 window=QWidget()
 window.setWindowTitle("Teste")
 l1=QLabel()
 layout = QGridLayout()
+
+
 for i in range(9):
+    a=randrange(0,10)
     for j in range(9):
-        layout.addWidget(QLabel(f"{i+1}"), i, j)
+        layout.addWidget(QLineEdit(f"{a}"), i, j)
 
 
 window.setLayout(layout)
 window.show()
 
 sys.exit(app.exec_())
-
