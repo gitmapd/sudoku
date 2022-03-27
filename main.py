e1716525ffbe804b05a267017f3220e1c5f5422d
gitmapd <50059062+gitmapd@users.noreply.github.com>
2022-03-27 21:11:56 +0100

    new loop

--- main.py
+++ main.py
@@ -4,8 +4,6 @@ from PyQt5.QtWidgets import QApplication, QLabel
 from PyQt5.QtWidgets import QWidget, QGridLayout, QLineEdit
 from random import randrange
 
-from numpy import square
-from torch import square_
 app=QApplication(sys.argv)
 window=QWidget()
 window.setWindowTitle("Sudoku")
@@ -14,15 +12,14 @@ layout = QGridLayout()
 
 square_values = [
     [5, 3, None, None, 7, None, None, None, None],
-    [6, None, None, 1, 9, 5, None, None, None,...]]
+    [6, None, None, 1, 9, 5, None, None, None]]
 
 for i in range(9):
     for j in range(9):
-        a=[x for x in square_values]
-        print(a)
-        lineedit=QLineEdit(f"{*a,}")
+        a=[x for sublist in square_values for x in sublist]
+        lineedit=QLineEdit(f"{a[i]}")
         lineedit.setFixedWidth(20)
-        lineedit.setReadOnly(True)
+        lineedit.setReadOnly(False)
         #   lineedit.move(20 + 40 * j, 20 + 40 * i)
         layout.addWidget(lineedit,i,j)
 window.setLayout(layout)
